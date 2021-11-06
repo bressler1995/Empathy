04b71d144fc20490322013bed64d83194b473953
carlofabyss <carlofabyss@hotmail.com>
2021-11-06 15:19:32 -0700

    Change to port 3000

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+var io = require('socket.io')(server, {'transports': ['websocket', 'polling']});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
@@ -15,11 +14,13 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
+    var clients = io.sockets.clients();
+    console.log(clients);
   });
 
-  socket.on('chat message', (msg) => {
+  socket.on('chat_message', (msg) => {
     console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    io.emit('chat_message', msg);
   });
 
 });
